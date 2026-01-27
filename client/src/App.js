048b76148bceeaeca48d843587b9b65b09bf12de
Li200858 <18211080345@163.com>
2026-01-28 07:23:12 +0800

    修改按钮文字：选择社团 -> 查看社团时间

--- client/src/App.js
+++ client/src/App.js
@@ -155,7 +155,7 @@ function App() {
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
-              选择社团
+              查看社团时间
             </a>
             <p className="text-sm text-gray-600 font-medium flex-1">
               点击此链接来选择社团，此功能等待网站更新
